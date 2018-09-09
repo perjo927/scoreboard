997080b076830d21b1e9e4e81ae2c931e52e3a31
perjo927 <perjo927@student.liu.se>
2018-09-09 12:54:18 +0200

    develop: add chart

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
-import Gun from 'gun/gun';
 import hyperHTML from "hyperhtml";
 import moment from "moment";
-
-const gun = Gun();
-
-// gun.get('per').put({
-//   name: "Per",
-//   score: "0",
-// });
-
-// gun.get('per').on((data, key) => {
-//   console.log("update:", data);
-// });
+import Chartist from "chartist";
 
 function tick(render) {
-    render`
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is ${moment().format('MMMM Do YYYY, h:mm:ss a')}.</h2>
-      </div>
-    `;
+  render`
+    <div>
+      ${moment("20181231", "YYYYMMDD").fromNow()}.
+    </div>
+  `;
 }
-  
-setInterval(tick, 5000,
-    hyperHTML.bind(document.getElementById('root')));
+setInterval(tick, 1000,
+  hyperHTML(document.getElementById('root'))
+);
+
+var data = {
+  series: [[
+    {x: 36, y: 0},
+    {x: 37, y: 100}
+  ]]
+};
+
+var options = {
+  axisX: {
+    type: Chartist.AutoScaleAxis,
+    onlyInteger: true,
+    low: 36,
+    high: 52
+  },
+  axisY: {
+    type: Chartist.FixedScaleAxis,
+    ticks: [0, 25, 50, 75, 100, 125, 150],
+    low: 0,
+    high: 150
+  },
+  showArea: true
+};
+
+new Chartist.Line('.ct-chart', data, options);
